08c653c2720fabd829ec3fa51222988f0bc4c972
NikaKhiz <khizambarelinika123@gmail.com>
2022-11-26 23:56:35 +0400

    Refactor initial setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import Tours from './Tours'
 
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+  }
+  useEffect(() => {
+    fetchTours();
+  })
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
 }
 
 export default App
